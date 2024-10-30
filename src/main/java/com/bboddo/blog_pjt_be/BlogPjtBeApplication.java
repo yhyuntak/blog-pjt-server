3c55cf64c1fb69e1c442c1e419bcacea8c30afd6
yhyuntak <yhyuntak@gmail.com>
2024-10-31 08:18:41 +0900

    [ADD] initial settings

---
+++ src/main/java/com/bboddo/blog_pjt_be/BlogPjtBeApplication.java
@@ -0,0 +1,13 @@
+package com.bboddo.blog_pjt_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPjtBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPjtBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bboddo/blog_pjt_be/BlogPjtBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bboddo.blog_pjt_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPjtBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
